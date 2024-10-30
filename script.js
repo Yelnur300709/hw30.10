8750c55e7d8f16ff603eb4d29ce53e58c674036d
Yelnur300709 <mirasleha8@gmail.com>
2024-10-31 00:43:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Көлік моделі: Daewoo Matiz");
+console.log("Көлік түсі: Жасыл");
+console.log("Дөңгелек саны: 4");
+console.log("Жолсыз аймақпен жүруге арналуы: false");
+console.log("Рейтингі: 3.5");
